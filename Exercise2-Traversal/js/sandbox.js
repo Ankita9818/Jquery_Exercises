58a3bc23a51a667c3e73fa4aa511b55f05505573
Ankita9818 <ankita.dixit@vinsol.com>
2017-10-30 11:12:11 +0530

    optimised code and did changes as per suggestions

--- Exercise2-Traversal/js/sandbox.js
+++ Exercise2-Traversal/js/sandbox.js
@@ -5,21 +5,16 @@ $(document).ready(function() {
   });
 
   //2- Select the search input text box, then traverse up to the form and add a class to the form.
-  $("input.input_text").parent().addClass("bordered-form");
+  $("input.input_text").parent("form#search").addClass("bordered-form");
 
   //3- Select the list item inside #myList that has a class of "current" and remove that class from it;
   //add a class of "current" to the next list item.
-  var $currentListItem = $("#myList li.current");
-  $currentListItem.removeClass("current");
-  $currentListItem.next().addClass("current");
+  $("#myList li.current").removeClass("current").next().addClass("current");
 
   //4- Select the select element inside #specials; traverse your way to the submit button.
-  var $selectElement = $("#specials select");
-  var $submitBtn = $selectElement.parent().next().children();
+  console.log($("#specials select").parent("li").next().children());
 
   //5- Select the first list item in the #slideshow element; add the class "current" to it,
   //and then add a class of "disabled" to its sibling elements.
-  var $firstSlideshowElement = $("#slideshow li:first");
-  $firstSlideshowElement.addClass("current");
-  $firstSlideshowElement.siblings().addClass("disabled");
+  $("#slideshow li:first").addClass("current").siblings().addClass("disabled");
 });
