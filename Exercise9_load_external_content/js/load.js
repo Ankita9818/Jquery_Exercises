fc85f12669080e128994d1d58b3f0f9a803663d2
Ankita9818 <ankita.dixit@vinsol.com>
2017-10-31 15:50:39 +0530

    Exercise9- changed to oop

--- Exercise9_load_external_content/js/load.js
+++ Exercise9_load_external_content/js/load.js
@@ -1,14 +1,30 @@
-$(document).ready(function() {
-  $("div#blog").find('h3').each(function() {
-    var $targetDiv = $('<div class="blogContent"></div>');
-    $(this).after($targetDiv).data('blogContent', $targetDiv);
-    $(this).bind('click',function(event) {
-      var href = $(this).find('a').attr('href'),
-        tempArray = href.split('#'),
-        id = '#' + tempArray[1];
-      $(this).data('blogContent').load('blog.html ' + id);
-      event.preventDefault();
-    });
-  });
-});
+function Load(options) {
+  this.blogHeadlines = options.blogHeadlines;
+}
 
+Load.prototype.init = function() {
+  var _this = this;
+  this.blogHeadlines.each(function() {
+    var $currentBlog = $(this),
+      $targetDiv = $('<div class="blogContent"></div>');
+    $currentBlog.after($targetDiv).data('blogContent', $targetDiv);
+    _this.addclickHandler($currentBlog);
+  });
+}
+
+Load.prototype.addclickHandler = function($currentBlogItem) {
+  $currentBlogItem.click(function(event) {
+    var href = $currentBlogItem.find('a').attr('href'),
+      id = '#' + href.split('#')[1];
+    $currentBlogItem.data('blogContent').load('blog.html ' + id);
+    event.preventDefault();
+  });
+}
+
+$(document).ready(function() {
+  var options = {
+    blogHeadlines : $("div#blog h3")
+  },
+    loadContentObject = new Load(options);
+  loadContentObject.init();
+});
