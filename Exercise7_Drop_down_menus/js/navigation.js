3ab451cc4703f86a66034f5190c852797d86f740
Ankita9818 <ankita.dixit@vinsol.com>
2017-10-31 11:42:56 +0530

    changed to oop

--- Exercise7_Drop_down_menus/js/navigation.js
+++ Exercise7_Drop_down_menus/js/navigation.js
@@ -1,11 +1,26 @@
+function Navigation(options) {
+  this.navbarItems = options.navbarItems;
+  this.className = options.classToAdd;
+  this.submenuSelector = options.submenuSelector;
+}
+
+Navigation.prototype.init = function() {
+  var _this = this;
+  this.navbarItems.hover(
+    function() {
+      $(this).addClass(_this.className).find(_this.submenuSelector).show();
+    }, function() {
+      $(this).removeClass(_this.className).find(_this.submenuSelector).hide();
+    }
+  );
+};
+
 $(document).ready(function() {
-  $("ul#nav").children().each(function() {
-    $(this).hover(
-      function() {
-        $(this).addClass("hover").children("ul").show();
-      }, function() {
-        $(this).removeClass("hover").children("ul").hide();
-      }
-    );
-  });
+  var options = {
+    navbarItems : $("ul#nav li"),
+    classToAdd : "hover",
+    submenuSelector : "ul"
+  };
+  var navigationObject = new Navigation(options);
+  navigationObject.init();
 });
