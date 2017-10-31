b11f833bacf47503b0c955abf386d5cf6118d8d7
Ankita9818 <ankita.dixit@vinsol.com>
2017-10-31 12:38:27 +0530

    changed to OOP

--- Exercise4_Input_hint/js/inputHint.js
+++ Exercise4_Input_hint/js/inputHint.js
@@ -1,18 +1,40 @@
-$(document).ready(function() {
-  var $formSearch = $('form#search');
-  var $searchInput = $formSearch.find('input[type=text]');
-  var $hintText = $formSearch.find('label').remove().text();
-  $searchInput.attr('value', $hintText).addClass('hint');
+function Hint(options) {
+  this.formSearch = $(options.formName);
+  this.searchInput = this.formSearch.find(options.searchInputName);
+  this.searchLabel = this.formSearch.find(options.searchLabelName);
+  this.classToAdd = options.classToAdd;
+}
 
-  //focus event handler
-  $searchInput.bind('focus',function() {
-    $(this).val('').removeClass('hint');
+Hint.prototype.init = function() {
+  var hintText = this.searchLabel.remove().text();
+  this.searchInput.attr('value', hintText).addClass(this.classToAdd);
+  this.focus();
+  this.blur(hintText);
+};
+
+Hint.prototype.focus = function() {
+  var _this = this;
+  this.searchInput.bind('focus',function() {
+    $(this).val('').removeClass(_this.classToAdd);
   });
+};
 
-  //blur event Handler
-  $searchInput.bind('blur', function() {
+Hint.prototype.blur = function(hintText) {
+  var _this = this;
+  this.searchInput.bind('blur', function() {
     if(!$(this).val()) {
-      $(this).val($hintText).addClass('hint');
+      $(this).val(hintText).addClass(_this.classToAdd);
     }
   });
+};
+
+$(document).ready(function() {
+  var options = {
+    formName : "#search",
+    searchInputName : "input[type=text]",
+    searchLabelName : "label",
+    classToAdd : "hint"
+  },
+    hintObject = new Hint(options);
+  hintObject.init();
 });
