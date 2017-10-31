6cd1d683efc1bbef73a88a9fb9cfb0ebf25d33fb
Ankita9818 <ankita.dixit@vinsol.com>
2017-10-31 12:44:36 +0530

    changed to OOP

--- Exercise4_Input_hint/js/inputHint.js
+++ Exercise4_Input_hint/js/inputHint.js
@@ -1,29 +1,29 @@
-function Hint(options) {
+function InputHint(options) {
   this.formSearch = $(options.formName);
   this.searchInput = this.formSearch.find(options.searchInputName);
   this.searchLabel = this.formSearch.find(options.searchLabelName);
   this.classToAdd = options.classToAdd;
 }
 
-Hint.prototype.init = function() {
+InputHint.prototype.init = function() {
   var hintText = this.searchLabel.remove().text();
   this.searchInput.attr('value', hintText).addClass(this.classToAdd);
   this.focus();
   this.blur(hintText);
 };
 
-Hint.prototype.focus = function() {
+InputHint.prototype.focus = function() {
   var _this = this;
   this.searchInput.bind('focus',function() {
-    $(this).val('').removeClass(_this.classToAdd);
+    _this.searchInput.val('').removeClass(_this.classToAdd);
   });
 };
 
-Hint.prototype.blur = function(hintText) {
+InputHint.prototype.blur = function(hintText) {
   var _this = this;
   this.searchInput.bind('blur', function() {
-    if(!$(this).val()) {
-      $(this).val(hintText).addClass(_this.classToAdd);
+    if(!_this.searchInput.val()) {
+      _this.searchInput.val(hintText).addClass(_this.classToAdd);
     }
   });
 };
@@ -35,6 +35,6 @@ $(document).ready(function() {
     searchLabelName : "label",
     classToAdd : "hint"
   },
-    hintObject = new Hint(options);
-  hintObject.init();
+    inputHintObject = new InputHint(options);
+  inputHintObject.init();
 });
