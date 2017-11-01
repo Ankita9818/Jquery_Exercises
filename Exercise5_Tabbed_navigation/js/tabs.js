59048b0dcf1ae3b45c565a9afa43444858f4a3ee
Ankita9818 <ankita.dixit@vinsol.com>
2017-11-01 16:46:19 +0530

    Exercise5 changed to OOP and seperated click handler

--- Exercise5_Tabbed_navigation/js/tabs.js
+++ Exercise5_Tabbed_navigation/js/tabs.js
@@ -1,21 +1,48 @@
-$(document).ready(function() {
-  var $divmodule = $('div.module');
-  $divmodule.hide();
-  var $newList = $('<ul></ul>', {
+function Tabs(options) {
+  this.$divModule = options.divmodule;
+  this.classToAdd = options.classToAdd;
+}
+
+Tabs.prototype.init = function() {
+  this.$divModule.hide();
+  this.$newList = $('<ul/>', {
     'class' : 'bordered-tab'
   });
-  $newList.insertBefore($divmodule.first());
-  $divmodule.each(function( index ) {
-    var $listItem = $('<li>' + $(this).find('h2').text() + '</li>');
-    $listItem.data('relatedDiv', $(this));
-    $listItem.click(function() {                    //Click event handler
-      $divmodule.hide();
-      $newList.children().removeClass('current');
-      $(this).data('relatedDiv').show();
-      $(this).addClass('current');
-    });
-    $newList.append($listItem);
+  this.$newList.insertBefore(this.$divModule.eq(0));
+  this.createList();
+  this.handleClick();
+  this.$divModule.eq(0).show();                        //To show the first tab by default
+  this.$newList.find('li:first').addClass(this.classToAdd);
+};
+
+Tabs.prototype.createList = function() {
+  var _this = this;
+  this.$divModule.each(function() {
+    var $currentDiv = $(this);
+    $('<li/>', {
+      text : $currentDiv.find('h2').text(),
+    })
+      .data('relatedDiv', $currentDiv)
+      .appendTo(_this.$newList);
   });
-  $divmodule.first().show();                        //To show the first tab by default
-  $newList.children().first().addClass('current');
+};
+
+Tabs.prototype.handleClick = function() {
+  var _this = this;
+  this.$newList.find('li').click(function() {
+    var $currentItem = $(this);
+    _this.$divModule.hide();
+    $currentItem.data('relatedDiv').show();
+    $currentItem.addClass(_this.classToAdd)
+      .siblings().removeClass(_this.classToAdd);
+  });
+};
+
+$(function() {
+  var options = {
+    divmodule : $('div.module'),
+    classToAdd : 'current'
+  },
+    tabbed_nav = new Tabs(options);
+  tabbed_nav.init();
 });
