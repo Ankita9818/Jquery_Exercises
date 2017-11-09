9d19d4008df1597a320c4d8cbd689e6d8f24acbb
Ankita9818 <ankita.dixit@vinsol.com>
2017-11-09 12:56:16 +0530

    changed to discussed structure

--- Exercise_arithmetic_quiz/js/quiz.js
+++ Exercise_arithmetic_quiz/js/quiz.js
@@ -49,58 +49,61 @@ function Quiz(options) {
   this.$questionNumber = this.$quizId.find(options.$questionNumber);
   this.$resultDiv = this.$quizId.find(options.$resultDiv);
   this.numberOfQuestions = options.numberOfQuestions;
-  this.score = 0;
+  this.currentScore = 0;
   this.correctAnswers = [];
-  this.questionObject = new Question(options.maximumNumber);
+  this.maxNumber = options.maximumNumber;
+  this.marks = options.marks;
 }
 
 //Function to initiate quiz
-Quiz.prototype.init = function() {
-  this.displayQuestion();
-  this.addClickHandler();
+Quiz.prototype.start = function() {
+  this.createQuestions();
+  this.displayQuestionOrResult();
+  this.evaluateResponse();
 };
 
-//Function which gets a question and displays it
-Quiz.prototype.displayQuestion = function() {
-  this.$resultDiv.hide();
+//Function which creates a question object
+Quiz.prototype.createQuestions = function() {
+  this.questionObject = new Question(this.maxNumber);
+};
+
+//Function which displays question and finally result screen
+Quiz.prototype.displayQuestionOrResult = function() {
+  (this.questionObject.questionNumber < this.numberOfQuestions) ? this.askQuestion() : this.displayFinalScreen();
+};
+
+Quiz.prototype.askQuestion = function() {
   this.question = this.questionObject.getQuestion();
-  if(this.questionObject.questionNumber > this.numberOfQuestions) {
-    this.displayScore();
-  }
   this.$questionNumber.html(this.questionObject.questionNumber);
-  this.$question.html(this.question.operands.firstOperand + ' ' + this.question.operands.operator
-    + ' ' + this.question.operands.secondOperand);
+  this.$question.html(this.question.operands.firstOperand + ' ' + this.question.operands.operator +
+    ' ' + this.question.operands.secondOperand);
   this.$answerInput.focus();
 };
 
 //Function which calculates score and gets next question
-Quiz.prototype.addClickHandler = function() {
+Quiz.prototype.evaluateResponse = function() {
   var _this = this;
   this.$nextBtn.click(function() {
-    if(_this.$answerInput.val() == _this.question.correctResult) {
-      _this.score += 1;
-      _this.question.decision = true;
-    }
-    else {
-      _this.question.decision = false;
+    if(_this.question.decision = (_this.$answerInput.val() == _this.question.correctResult)) {
+      _this.currentScore += _this.marks;
     }
     _this.correctAnswers.push(_this.question);
     _this.$answerInput.val('');
-    _this.$score.html('Your Score is ' + _this.score);
-    _this.displayQuestion();
+    _this.$score.html('Your Score is ' + _this.currentScore);
+    _this.displayQuestionOrResult();
   });
 };
 
-//Function to display the score to user along with correct answers for the questions answered wrong
-Quiz.prototype.displayScore = function() {
+//Function to display the final score to user along with correct answers for the questions answered wrong
+Quiz.prototype.displayFinalScreen = function() {
   this.$questionBlock.hide();
-  this.$resultDiv.show().append($('<p>').text('Your Score is ' + this.score));
+  this.$resultDiv.show().append(this.$score);
   this.$resultDiv.append($('<p>').text('Correct Answers for the questions which you answered wrong'));
   for (var index = 0; index < this.numberOfQuestions; index++) {
     if(!this.correctAnswers[index].decision) {
       var text = 'Question No. ' + (index + 1) + ' :-> ' + this.correctAnswers[index].operands.firstOperand + ' ' +
-        this.correctAnswers[index].operands.operator + ' ' + this.correctAnswers[index].operands.secondOperand
-        + " = " + this.correctAnswers[index].correctResult;
+        this.correctAnswers[index].operands.operator + ' ' + this.correctAnswers[index].operands.secondOperand +
+        " = " + this.correctAnswers[index].correctResult;
       this.$resultDiv.append($('<p>').text(text));
     }
   }
@@ -108,7 +111,6 @@ Quiz.prototype.displayScore = function() {
 
 //Constant for operators
 const OPERATOR_ARRAY = ['+','-','*','/'];
-
 $(function() {
   var options = {
     $quizId : $('#quiz'),
@@ -120,8 +122,10 @@ $(function() {
     $questionNumber : '.qno',
     $resultDiv : '.result',
     numberOfQuestions: 20,
-    maximumNumber : 20
+    maximumNumber : 20,
+    marks : 4
   },
     quiz = new Quiz(options);
-  quiz.init();
+  quiz.start();
 });
+
