ee7066e6e01e2ba654339d9543f47d1481aa25ea
Ankita9818 <ankita.dixit@vinsol.com>
2017-11-14 11:21:42 +0530

    Exercise 13 product display committed

---
+++ Exercise13_product_display/js/product_list.js
@@ -0,0 +1,79 @@
+function ProductList(options) {
+  this.$productContainer = options.$productContainer;
+  this.$filterBox = options.$filterBox;
+  this.urlToJsonFile = options.url;
+  this.filteredProducts = [];
+  this.allProducts = [];
+}
+
+ProductList.prototype.init = function() {
+  this.loadJsonData();
+  this.addClickEventHandler();
+};
+
+ProductList.prototype.loadJsonData = function(){
+  var _this = this;
+  $.ajax({
+    type : 'GET',
+    url : this.urlToJsonFile,
+    dataType : 'json',
+    success : function(response) {
+      $.each(response, function(index) {
+        _this.storeAllProducts(index, this);
+      });
+      _this.displayAllProducts();
+    }
+  });
+};
+
+ProductList.prototype.storeAllProducts = function(index, currentResponseObject) {
+  this.allProducts.push("<div id='" + (index + 1) + "' data-type='productimage' data-brand = '" +
+    currentResponseObject.brand + "' data-color = '" + currentResponseObject.color + "' data-sold_out = '" +
+    currentResponseObject.sold_out + "'><img src=images/" + currentResponseObject.url + " /></div>");
+};
+
+ProductList.prototype.displayAllProducts = function() {
+  this.$productContainer.append(this.allProducts);
+};
+
+ProductList.prototype.addClickEventHandler = function() {
+  var _this = this;
+  this.$filterBox.on("change", function() {
+    var $filterElements = _this.$productContainer.find("[data-type='productimage']");
+    $filterElements.hide();
+    $filterElements = _this.filterProducts($filterElements);
+    $filterElements.show();
+  });
+};
+
+ProductList.prototype.filterProducts = function(filterElements) {
+  var _this = this;
+  this.$filterBox.each(function() {
+    var $currentFilter = $(this),
+      checkedInput = $currentFilter.find("input[data-category='" + $currentFilter.attr("id") + "']:checked");
+    _this.filteredProducts = [];
+    if(checkedInput.length > 0) {
+      _this.saveFilteredProductsInArray(checkedInput, $currentFilter);
+      filterElements = filterElements.filter(_this.filteredProducts.join());
+    }
+  });
+  return filterElements;
+};
+
+ProductList.prototype.saveFilteredProductsInArray = function(checkedFilter, currentFilterBox) {
+  var _this = this;
+  checkedFilter.each(function() {
+    var id = currentFilterBox.attr('id');
+    _this.filteredProducts.push("[data-" + id + " = '" + $(this).attr('data-' + id) + "']");
+  });
+};
+
+$(function() {
+  var options = {
+    $productContainer : $("#product-container"),
+    $filterBox : $(".categories .filter-div"),
+    url : 'json/product.json'
+  },
+    productFilter = new ProductList(options);
+  productFilter.init();
+});
