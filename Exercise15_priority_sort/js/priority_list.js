f5ee573193a7f110558dacbf4f445700a1ab7c9d
Ankita9818 <ankita.dixit@vinsol.com>
2017-12-12 17:19:34 +0530

    separated function as discussed

--- Exercise15_priority_sort/js/priority_list.js
+++ Exercise15_priority_sort/js/priority_list.js
@@ -1,19 +1,31 @@
 function PriorityListManager(options) {
   this.$priorityList = options.$priorityList;
-  this.$allListItems = this.$priorityList.find('li');
+  this.$allListItems = this.$priorityList.find(options.allListItems);
   this.$initialItems = this.$allListItems.filter(options.priorityAttribute);
   this.initial_item_count = this.$priorityList.attr(options.initialItemCount);
+  this.dataPriorityOrder = options.dataPriorityOrder;
 }
 
 PriorityListManager.prototype.init = function() {
   this.$linkAll = this.appendLink('all');
   this.$linkLess = this.appendLink('less');
+  this.processListItems();
   this.displayInitialItems();
-  this.bindCLickListeners();
+  this.bindClickListeners();
+};
+
+//Function which adds data-attribute priority order to all list items
+PriorityListManager.prototype.processListItems = function() {
+  var _this = this;
+  $.each(this.$allListItems, function() {
+    if(!$(this).attr(_this.dataPriorityOrder)) {
+      $(this).data('priority-order', '9999');
+    }
+  });
 };
 
 //Function which binds listeners to links
-PriorityListManager.prototype.bindCLickListeners = function() {
+PriorityListManager.prototype.bindClickListeners = function() {
   var _this = this;
   this.$linkAll.on('click', function() {
     _this.displayAllItems();
@@ -23,10 +35,14 @@ PriorityListManager.prototype.bindCLickListeners = function() {
   });
 };
 
+PriorityListManager.prototype.getInitialItemsToDisplay = function() {
+  return this.$allListItems.slice(0, this.initial_item_count);
+};
+
 //Function to display Initial Items
 PriorityListManager.prototype.displayInitialItems = function() {
-  this.sortItems(this.$initialItems, true);
-  this.$initialItemsToShow = this.$initialItems.slice(0, this.initial_item_count);
+  this.sortByPriorityOrder(this.$allListItems);
+  this.$initialItemsToShow = this.getInitialItemsToDisplay();
   this.$allListItems.not(this.$initialItemsToShow).hide();
   this.$priorityList.append(this.$initialItemsToShow);
   this.displayLinks(this.$linkLess, this.$linkAll);
@@ -35,7 +51,7 @@ PriorityListManager.prototype.displayInitialItems = function() {
 //Function to display all Items
 PriorityListManager.prototype.displayAllItems = function() {
   this.$allListItems.show();
-  this.sortItems(this.$allListItems, false);
+  this.sortAlphabetically(this.$allListItems);
   this.$priorityList.append(this.$allListItems);
   this.displayLinks(this.$linkAll, this.$linkLess);
 };
@@ -50,7 +66,7 @@ PriorityListManager.prototype.displayLinks = function(hiddenLink, visibleLink) {
 PriorityListManager.prototype.appendLink = function(linkText) {
   var link = $('<a>', {
     'text' : ('See ' + linkText),
-    'href' : '#',
+    'href' : 'javascript:void(0)',
     'data-id' : linkText,
     'class' : 'link'
   });
@@ -59,13 +75,22 @@ PriorityListManager.prototype.appendLink = function(linkText) {
 };
 
 //Function which sorts list items
-PriorityListManager.prototype.sortItems = function($listItems, priorityFlag) {
+PriorityListManager.prototype.sortByPriorityOrder = function($listItems) {
   var _this = this;
   $listItems.sort(function(elem1, elem2) {
-    var sortCondition = (priorityFlag) ?
-       _this.getPriorityOrder(elem1) > _this.getPriorityOrder(elem2) :
-       _this.getContent(elem1) > _this.getContent(elem2);
-    if(sortCondition) {
+    if(_this.getPriorityOrder(elem1) > _this.getPriorityOrder(elem2)) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+};
+
+//Function which sorts list items Alphabetically
+PriorityListManager.prototype.sortAlphabetically = function($listItems) {
+  var _this = this;
+  $listItems.sort(function(elem1, elem2) {
+    if(_this.getContent(elem1) > _this.getContent(elem2)) {
       return 1;
     } else {
       return -1;
@@ -75,19 +100,21 @@ PriorityListManager.prototype.sortItems = function($listItems, priorityFlag) {
 
 //Function which returns priority value of list item
 PriorityListManager.prototype.getPriorityOrder = function(element) {
-  return ($(element).attr('data-priority-order'));
+  return ($(element).data('priority-order'));
 };
 
 //Function which returns text of item
 PriorityListManager.prototype.getContent = function(element) {
-  return ($(element).text().toLowerCase());
+  return ($(element).data('value').toLowerCase());
 };
 
 $(function() {
   var options = {
     $priorityList : $('[data-id="priority-sort"]'),
+    allListItems : '[data-id="list-item"]',
     priorityAttribute : '[data-priority-order]',
-    initialItemCount : 'data-initial-item-count'
+    initialItemCount : 'data-initial-item-count',
+    dataPriorityOrder : 'data-priority-order'
   },
     list = new PriorityListManager(options);
   list.init();
